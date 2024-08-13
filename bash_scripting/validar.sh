e4a564b97a707926f7c99396412461e893c7b5bc
Fernando125 <fernandogomez125@gmail.com>
2024-08-13 20:20:16 +0200

    Fix expressions

--- bash_scripting/validar.sh
+++ bash_scripting/validar.sh
@@ -13,24 +13,23 @@
 validar() {
     pass="$1"
 
-    if [[ ${#pass} -lt 8 ]]; then
+    if [ -z "$1" ]; then
+        echo "No se ha pasado ninguna contraseña, pruebe de nuevo"
+        exit
+    elif [[ ${#pass} -lt 8 ]]; then
         echo "La password es debil, debe tener al menos 8 caracteres"
         return
-    fi
-
-    if ! [[ "$pass" =~ [0-9] ]]; then
+    elif ! [[ "$pass" =~ [0-9] ]]; then
         echo "La contraseña es debil, debe contener al menos un numero"
         return
-    fi
-
-    if ! [[ "$pass" =~ [@#$%&*+\-=] ]]; then
+    elif ! [[ "$pass" =~ [@#$%*+\-=] ]]; then
         echo "La contraseña es debil, debe contener al menos un caracter especial"
         return
     fi
 
     echo "La contraseña es valida"
+    
 }
 
 pass="$1"
-
 validar "$pass"
